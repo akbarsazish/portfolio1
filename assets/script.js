80a992939d580cf696c943b0c2330333defc16a7
akbarsazish <sazishaliakbar@gmail.com>
2023-05-22 16:09:47 -0700

    Add font family and font weight to mobile menu items

--- assets/script.js
+++ assets/script.js
@@ -2,6 +2,7 @@ function openMobileMenu(){
     document.getElementById("mobile-menu").style.width = "100%";
     document.getElementById("close-icon").style.display = "block";
 }
+
 function closeMobileMenu(){
     document.getElementById("mobile-menu").style.width = "0%";
     document.getElementById("close-icon").style.display = "none";
