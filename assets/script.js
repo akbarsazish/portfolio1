9ab38ed7cf921e26dd41f1aeac59be5dbf24eb51
akbarsazish <sazishaliakbar@gmail.com>
2023-05-22 14:50:19 -0700

    Add style on mobile menu

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,8 @@
 function openMobileMenu(){
     document.getElementById("mobile-menu").style.width = "100%";
+    document.getElementById("close-icon").style.display = "block";
 }
 function closeMobileMenu(){
     document.getElementById("mobile-menu").style.width = "0%";
+    document.getElementById("close-icon").style.display = "none";
 }
